ff2b568ecd043a60d95d3e3cbe9998d97ae7ad1a
Amit Pawar <amit.pawar410@gmail.com>
2022-02-28 13:59:33 -0500

    Check for duplicate number, return true if present otherwise false.

---
+++ src/leetcode/ContainDuplicates.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainDuplicates {
+    public boolean containsDuplicate(int[] nums) {
+        Set <Integer> st = new HashSet <Integer> ();
+        for (int num:nums){
+            if (!st.add (num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Test
+    public void testTrue () {
+        int [] nums ={1,2,3,1};
+        Assert.assertTrue("True",containsDuplicate(nums));
+    }
+
+    @Test
+    public void testFalse () {
+        int [] nums ={1,2,3,4};
+        Assert.assertFalse("True",containsDuplicate(nums));
+    }
+
+    @Test
+    public void True1 () {
+        int [] nums ={1,1,1,3,3,4,3,2,4,2};
+        Assert.assertTrue("True",containsDuplicate(nums));
+    }
+}
+
